dceb07137a410ddf634cb92cd46dbba7fd5becd8
Alexandr Bogomyakov <ultradesu@hexor.ru>
2024-09-28 20:44:08 +0300

    Update windows_task.ps1

--- windows_task.ps1
+++ windows_task.ps1
@@ -1,5 +1,5 @@
 if ($args.Count -lt 2) {
-    Log-Message "Usage: windows_task.ps1 <url> <sslocal_path> <comment>"
+    Write-Host "Usage: windows_task.ps1 <url> <sslocal_path> <comment>"
     exit 1
 }
 
@@ -29,7 +29,7 @@ function Start-SSLocal {
     Get-Process sslocal -ErrorAction SilentlyContinue | Stop-Process -Force
 
     # Log the sslocal restart
-    Log-Message "Starting sslocal with method: $method, server: $server, port: $serverPort"
+    Write-Host "Starting sslocal with method: $method, server: $server, port: $serverPort"
 
     # Start sslocal with the provided arguments
     Start-Process -NoNewWindow -FilePath $sslocalPath -ArgumentList "--local-addr ${localAddr}:${localPort} --server-url $ssUrl"
@@ -49,20 +49,20 @@ while ($true) {
         $serverPort = $json.server_port
 
         # Log current password and server information
-        Log-Message "Checking server: $server, port: $serverPort"
+        Write-Host "Checking server: $server, port: $serverPort"
 
         # Check if the password has changed
         if ($password -ne $previousPassword) {
             # Start/restart sslocal
             Start-SSLocal -method $method -password $password -server $server -serverPort $serverPort
             $previousPassword = $password
-            Log-Message "Password changed, restarting sslocal."
+            Write-Host "Password changed, restarting sslocal."
         } else {
-            Log-Message "Password has not changed."
+            Write-Host "Password has not changed."
         }
 
     } catch {
-        Log-Message "Error occurred: $_"
+        Write-Host "Error occurred: $_"
     }
 
     # Wait for the next check
